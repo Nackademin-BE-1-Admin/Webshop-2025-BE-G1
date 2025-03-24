7cb89af2815f75b3d88c4b4cc7d63ee09db87216
Gorilliam <gorilliam99@icloud.com>
2025-03-24 10:51:29 +0100

    Created first draft of product schema with reference to category schema

--- src/models/Product.js
+++ src/models/Product.js
@@ -11,6 +11,29 @@ const productSchema = new mongoose.Schema({
     required: true,
     min: 0
   },
+  image: {
+    type: String,
+    default: ""  //Add a picture URL to something that will become a placeholder
+  },
+  unit: {  //Kilos, litres etc...
+    type: String,
+    enum: ['kg', 'L'],
+    required: true
+  },
+  amount: {   //The number shown before unit type for a product
+    type: Number,
+    required: true
+  },
+  brand:  {
+    type: String,
+    required: true
+  },
+  discount: {
+    type: Number,
+    default: 1,
+    min: 0,
+    max: 1
+  },
   description: {
     type: String,
     default: ''
@@ -19,6 +42,12 @@ const productSchema = new mongoose.Schema({
     type: Number,
     default: 0,
     min: 0
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'category',
+    required: true
+
   }
 }, {
   timestamps: true
